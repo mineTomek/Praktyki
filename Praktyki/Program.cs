aee68f91ac7ae967cef9fb4aea2d725b4005678d
mineTomek <lukom.tomek@gmail.com>
2022-08-20 22:01:06 +0200

    Created project

---
+++ Praktyki/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool exit = false;
+
+        while (exit != true)
+        {
+            Console.Write("Write date of meeting: ");
+
+            string date = Console.ReadLine();
+
+            if (date == null) {
+                Console.WriteLine("Date can't be null!");
+                continue;
+            }
+
+            Console.WriteLine(
+                new Regex("^(0?[1-9]|1[0-2])[\\.](0?[1-9]|[12]\\d|3[01])[\\.]2\\d{3}\\b").IsMatch(date) ?
+                $"Date {date} matches date regex" :
+                $"Date {date} doesn't match date regex"
+            );
+        }
+    }
+}
