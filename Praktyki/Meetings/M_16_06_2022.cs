77cd803a13b71d9d4acf83f16a38c411ecefcba6
mineTomek <lukom.tomek@gmail.com>
2022-08-27 20:09:35 +0200

    Update M_16_06_2022.cs
    Added bubble sorting algorithm

--- Praktyki/Meetings/M_16_06_2022.cs
+++ Praktyki/Meetings/M_16_06_2022.cs
@@ -1,5 +1,7 @@
 ﻿using static Praktyki.Utilities.ConsoleColors;
 using static Praktyki.Utilities.AllowedColor;
+using Praktyki.Utilities;
+using System.Text;
 
 namespace Praktyki.Meetings
 {
@@ -9,7 +11,8 @@ namespace Praktyki.Meetings
         {
             { "Bool z int'a (0 / 1)", BoolFromInt },
             { "Switch'e z case'ami bez breake'a", SwitchesWithoutBrakes },
-            { "Int poza granicami", IntBeyondBoundries }
+            { "Int poza granicami", IntBeyondBoundries },
+            { "Sortowanie cyfr", Sorting }
         };
 
         static void BoolFromInt()
@@ -103,6 +106,194 @@ Jest to minimalna wartość &g'int&''a + 1.
 Stało się tak dlatego, że operacja doszła do maksymalnej wartości i kontynuowała liczenie od najmniejszej wartości.
 ");
         }
+
+        #region Sorting
+
+        static void Sorting()
+        {
+            ConsoleMenu.MenuOption[] options = new[]
+            {
+                new ConsoleMenu.MenuOption("Bubble Sorting", DarkCyan, Cyan),
+                new ConsoleMenu.MenuOption("Insert Sorting", DarkYellow, Yellow),
+                new ConsoleMenu.MenuOption("Merge Sorting"),
+                new ConsoleMenu.MenuOption("Exit", DarkRed, Red)
+            };
+
+            (int selectedIndex, _) = new ConsoleMenu(options).Show();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    BubbleSorting();
+                    break;
+                case 1:
+                    InsertSorting();
+                    break;
+                case 2:
+                    MergeSorting();
+                    break;
+                default:
+                    return;
+            }
+
+            StandardPause();
+
+            Sorting();
+        }
+
+        static List<int> GetIntsForSorting()
+        {
+            Write("Enter numbers: ", Gray);
+
+            string rawString = ReadLine(Yellow);
+
+            if (string.IsNullOrWhiteSpace(rawString))
+            {
+                Clear();
+                WriteLine("Entry cannot be empty", Red);
+                return GetIntsForSorting();
+            }
+
+            bool isBad = false;
+
+            List<int> ints = new List<int>();
+
+            rawString.Split(' ').ToList().ForEach((string str) =>
+            {
+                foreach (char chr in str)
+                {
+                    if (!(char.IsDigit(chr) || chr == '-'))
+                    {
+                        isBad = true;
+                    }
+                }
+
+                if (!isBad)
+                {
+                    ints.Add(int.Parse(str));
+                }
+            });
+
+            if (isBad)
+            {
+                Clear();
+                WriteLine("This is not fully a number", Red);
+                return GetIntsForSorting();
+            }
+
+            return ints;
+        }
+
+        static void BubbleSorting()
+        {
+            Clear();
+
+            WriteLine("Bubble Sorting:", Green);
+
+            List<int> ints = GetIntsForSorting();
+
+            bool? swapped = null;
+
+            while (swapped != false)
+            {
+                swapped = false;
+
+                WriteLine(StringifyList(ints));
+
+                for (int i = 0; i < ints.Count - 1; i++)
+                {
+                    
+                    if (ints[i] > ints[i + 1])
+                    {
+                        swapped = true;
+                        (ints[i], ints[i + 1]) = (ints[i + 1], ints[i]); // Swap
+
+                        WriteFromString($"&y'{StringifyList(GetRange(ints, 0, i - 1))}&c'{ints[i]} {ints[i + 1]} &y'{StringifyList(GetRange(ints, i + 2, ints.Count - 1))}");
+                    } else
+                    {
+                        WriteFromString($"&w'{StringifyList(GetRange(ints, 0, i - 1))}&c'{ints[i]} {ints[i + 1]} &w'{StringifyList(GetRange(ints, i + 2, ints.Count - 1))}");
+                    }
+                }
+            }
+
+
+            WriteLine("Final list: " + StringifyList(ints), Green);
+        }
+
+        static void BubbleEnumerate(List<int> ints, int delay, int curentIndex = 0)
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(delay);
+            });
+
+            BubbleEnumerate(ints, delay, curentIndex + 1);
+        }
+
+        static void InsertSorting()
+        {
+            Clear();
+
+            WriteLine("Bubble Sorting:", Green);
+
+            List<int> ints = GetIntsForSorting();
+
+            WriteLine("Final list: " + StringifyList(ints), Green);
+        }
+
+        static void MergeSorting()
+        {
+            Clear();
+
+            WriteLine("Bubble Sorting:", Green);
+
+            List<int> ints = GetIntsForSorting();
+
+            
+
+            WriteLine("Final list: " + StringifyList(ints), Green);
+        }
+
+        static string StringifyList<T>(List<T> list, string separator = " ")
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (T item in list)
+            {
+                stringBuilder.Append(string.Concat(item.ToString(), separator));
+            }
+
+            string output = stringBuilder.ToString();
+
+            try
+            {
+                output.Substring(0, output.Length - 2);
+            } catch
+            {
+                return "";
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        static List<T> GetRange<T>(List<T> list, int fromIndex, int toIndex)
+        {
+            return list.GetRange(fromIndex, toIndex - fromIndex + 1);
+
+            /*
+            
+            2 5 => 4
+                v     v
+            0 1 2 3 4 5 6 7 8 9
+                ^^^^^^^
+
+            5 - 2 + 1
+
+            */ 
+           
+        }
+
+        #endregion
     }
 }
 
